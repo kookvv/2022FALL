3a818fb38a0bb7cc5f91231b5a1ed61bdd8957f1
kookvv <19220233@njnu.edu.cn>
2022-10-20 11:33:50 +0800

    Add homework-C2022FALLW02

---
+++ 2022W02/2-2.c
@@ -0,0 +1,20 @@
+﻿#include<stdio.h>
+int main()
+{
+	char a = 'B', b = 33;
+	char c = '0', d = '9';
+	a = a - 'A' + '0';
+	b = b * 2;
+	printf("%c,%c\n", a, b);
+	c++, d--;
+	printf("%c,%c\n", c, d);
+
+	int i = 8, j = 10;
+	int m = ++i;
+	int n = j++;
+	int x = -i++;
+	int y = j + 12 / ++j;
+	printf("%d,%d,%d,%d\n", i, j, m, n);
+	printf("%d,%d,%d,%d\n", i, j, x, y);
+	return 0;
+}
