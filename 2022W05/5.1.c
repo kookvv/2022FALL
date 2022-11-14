aeabaf7e21515278312721630f5de894e87eea5c
kookvv <19220233@njnu.edu.cn>
2022-11-14 22:24:54 +0800

    Add homework-C2022FALLW05

---
+++ 2022W05/5.1.c
@@ -0,0 +1,18 @@
+﻿#include<stdio.h>
+int is_sorted(int arr[], int left, int right)
+{
+	while (left < right)
+	{
+		if (arr[left] > arr[left + 1])
+			return 0;
+	}
+	return 1;
+}
+int main()
+{
+	int arr[10] = { 0,1,2,3,4,5,6,7,8,9 };
+	if (is_sorted(arr, 0, 3) == 1)
+		printf("yes");
+	else printf("no");
+	return 0;
+}
---
+++ 2022W05/5.14.c
@@ -0,0 +1,25 @@
+﻿//https://www.acwing.com/problem/content/submission/code_detail/19023974/
+int* specialSort(int N, int* returnSize) {
+    *returnSize = N;
+    int* ans = (int*)malloc(sizeof(int) * N);
+    ans[0] = 1;
+    for (int i = 2; i <= N; ++i)
+    {
+        int l = 0, r = i - 2;
+        while (l < r)
+        {
+            int mid = (l + r + 1) >> 1;
+            if (compare(ans[mid], i))
+                l = mid;
+            else
+                r = mid - 1;
+        }
+        for (int j = i - 1; j > l; --j)
+            ans[j] = ans[j - 1];
+        if (l == 0 && compare(i, ans[0]))
+            ans[l] = i;
+        else
+            ans[l + 1] = i;
+    }
+    return ans;
+}
